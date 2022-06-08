ca5538c390f6244d69421d6379e3457741eea289
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-06-08 22:29:01 +0200

    [FEAT] Complete app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Product } from './click-item-lista/shared/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-examples';
+  clickedProduct: Product;
+  onRecivedClickedProduct(data: Product) {
+    console.log(data);
+    this.clickedProduct = data;
+  }
 }
