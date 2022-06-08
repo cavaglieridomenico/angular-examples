b92bfcf1d72ea448a9c8573c5cf7ee6277f017f7
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-06-08 19:05:10 +0200

    [FEAT] App component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClickItemListaComponent } from './click-item-lista/click-item-lista.component';
+import { ItemComponent } from './click-item-lista/item/item.component';
+import { ItemDetailComponent } from './src/app/click-item-lista/item-detail/item-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ClickItemListaComponent, ItemComponent, ItemDetailComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
